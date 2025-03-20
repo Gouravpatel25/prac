584a3e5acb9df6894ff0ae482fbf0fdbdc92d7a6
GOURAV <gouravpatel20032003@gmail.com>
2025-03-20 14:34:04 +0530

    2nd Commit

--- Program1.java
+++ Program1.java
@@ -2,9 +2,9 @@ public class Program1{
 
 	public static void main(String[] args){
 		int a=10,b=20;
-		a=a+b;
-		b=a-b;
-		a=a-b;
+		int temp=a;
+		b=a;
+		a=temp;
 		System.out.println(a);
 		System.out.println(b);
 	} 

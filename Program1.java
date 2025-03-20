a9c703cc7f909a83bf85d6f1107fba41037d3d8b
GOURAV <gouravpatel20032003@gmail.com>
2025-03-20 15:12:47 +0530

    Changes done for reverse

--- Program1.java
+++ Program1.java
@@ -1,11 +1,9 @@
 public class Program1{
 
 	public static void main(String[] args){
-		int a=10,b=20;
-		int temp=a;
-		b=a;
-		a=temp;
-		System.out.println(a);
-		System.out.println(b);
+		for(int i=10; i>=0; i--){
+			System.out.print(i+" ");
+		}
+		System.out.println();
 	} 
 }
